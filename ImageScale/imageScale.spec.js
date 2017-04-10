7ff5ff5b1c03ec7e4aaa81d44637b0bbfb76fc51
Rick Chen <rick@rickfrom1987.com>
2017-04-09 17:02:37 -0700

    image scale tests

--- ImageScale/imageScale.spec.js
+++ ImageScale/imageScale.spec.js
@@ -2,10 +2,28 @@ const assert = require('assert');
 const ImageScale = require('./imageScale');
 
 describe('imageScale', function() {
+
   const is = new ImageScale();
+
   describe('#solve()', function() {
-    it('should', function() {
-      assert(true);
+    const WIDTH = 100;
+    const HEIGHT = 100;
+    const MAX_DIM = 100;
+
+    it('should scale a square image', function() {
+      const result = is.scaleImage(100, 100, MAX_DIM);
+      assert.deepEqual(result, [100,100]);
     });
+
+    it('should scale a landscape image by width', function() {
+      const result = is.scaleImage(200, 100, MAX_DIM);
+      assert.deepEqual(result, [100,50]);
+    });
+
+    it('should scale a portrait image by height', function() {
+      const result = is.scaleImage(100, 200, MAX_DIM);
+      assert.deepEqual(result, [50,100]);
+    });
+
   });
 });
